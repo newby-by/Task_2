5148f7a16806fe9fd38156643fd3f20d6e9cc959
newby-by <practikum10@gmail.com>
2026-04-18 12:09:02 +0300

    Fix remove self from allure step

--- methods/user.py
+++ methods/user.py
@@ -7,7 +7,7 @@ class UserMethod:
     def __init__(self, url):
         self.url = url
 
-    @allure.step('Register a user with payload={payload} url={self.url}')
+    @allure.step('Register a user with payload={payload}')
     def register(self, payload):
         response = requests.post(
             url=self.url,
